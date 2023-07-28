2da9b07aea01763086cea43d9acfeddc53fc4b45
irci <ircurlicus123@gmail.com>
2023-07-28 15:31:34 +0200

    Namespace fixes and readme.md

--- ToDoLists.Core/ToDoItem.cs
+++ ToDoLists.Core/ToDoItem.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ToDoLists;
+﻿namespace ToDoLists.Core;
 
 public class ToDoItem
 {
--- ToDoLists.Core/ToDoListManager.cs
+++ ToDoLists.Core/ToDoListManager.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
+﻿using System.Text.Json;
 
-namespace ToDoLists;
+namespace ToDoLists.Core;
 
 public class ToDoListManager
 {
