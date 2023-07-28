bfd3f112f77492ddd56b221fdce8928d41bea1f0
irci <ircurlicus123@gmail.com>
2023-07-28 15:16:10 +0200

    Moved the ToDoList logic to separate project

--- ToDoLists/MainWindow.xaml.cs
+++ ToDoLists/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -13,153 +9,128 @@ namespace ToDoLists;
 /// </summary>
 public partial class MainWindow
 {
-    private List<ToDoItem> _itemsList;
+    private readonly ToDoListManager _itemsListManager;
 
     public MainWindow()
     {
-        _itemsList = new List<ToDoItem>();
-        ImportItemsList();
+        _itemsListManager = new ToDoListManager();
+        _itemsListManager.ImportItemsList();
         InitializeComponent();
         UpdateListBox();
     }
+    
+    #region MethodsCalledFromWindow
     
     private void ShowItem(object sender, SelectionChangedEventArgs e)
     {
         ClearPreviewBox();
 
-        if (listBox.SelectedIndex == -1)
-        {
-            return;
-        }
+        string itemTitle = GetSelectedItemTitle();
 
-        var item = (ListBox)sender;
-        var listBoxItem = item.SelectedItem?.ToString();
-
-        if (listBoxItem == null)
+        if (string.IsNullOrWhiteSpace(itemTitle))
         {
             return;
         }
         
-        ToDoItem itemFound = GetItemFromList(listBoxItem);
+        ToDoItem itemFound = _itemsListManager.GetItemFromList(itemTitle);
 
-        titleTextBox.Text = itemFound.Title;
-        descriptionTextBox.Text = itemFound.Description;
-        doneTextBox.Text = itemFound.Done.ToString();
-
-        doneTextBox.Foreground = itemFound.Done ? Brushes.Green : Brushes.Red;
+        DrawItemDetails(itemFound);
     }
-
-    private void ClearPreviewBox()
-    {
-        titleTextBox.Clear();
-        descriptionTextBox.Clear();
-        doneTextBox.Clear();
-    }
-
-    private void UpdateListBox()
-    {
-        listBox.Items.Clear();
-        
-        if (_itemsList.Count <= 0)
-        {
-            return;
-        }
-
-        foreach (ToDoItem item in _itemsList)
-        {
-            listBox.Items.Add(item.Title);
-        }
-    }
-
-
+    
     private void DeleteListItem(object sender, RoutedEventArgs e)
     {
-        if (listBox.SelectedIndex < 0)
+        string itemTitle = GetSelectedItemTitle();
+        
+        if (string.IsNullOrWhiteSpace(itemTitle))
         {
             return;
         }
-
-        var itemTitle = listBox.SelectedItem.ToString();
         
-        ToDoItem itemToRemove = GetItemFromList(itemTitle);
+        _itemsListManager.DeleteItem(itemTitle);
 
         listBox.SelectedIndex = -1;
-        _itemsList.Remove(itemToRemove);
+        
         UpdateListBox();
     }
-
-
+    
     private void CheckDoneItem(object sender, RoutedEventArgs e)
     {
-        if (listBox.SelectedIndex < 0) return;
-
-        var itemTitle = listBox.SelectedItem.ToString();
+        string itemTitle = GetSelectedItemTitle();
         
-        ToDoItem itemToCheck = GetItemFromList(itemTitle);
+        if (string.IsNullOrWhiteSpace(itemTitle))
+        {
+            return;
+        }
         
-        itemToCheck.Done = !itemToCheck.Done;
+        _itemsListManager.ChangeItemDoneStatus(itemTitle);
+        
         UpdateListBox();
     }
-
-    private ToDoItem GetItemFromList(string itemTitle)
-    {
-        ToDoItem? itemFound = _itemsList.Find(findItem => findItem.Title == itemTitle);
-
-        if (itemFound is null)
-        {
-            throw new Exception("Could not find selected item");
-        }
-
-        return itemFound;
-    }
-
+    
     private void OpenAddWindow(object sender, RoutedEventArgs e)
     {
         var addWindow = new AddItemWindow(parent: this);
         addWindow.Show();
     }
 
-    public void AddNewItem(string _title, string _description)
+    public void AddNewItem(string title, string description)
     {
-        var itemToAdd = new ToDoItem
-        {
-            Title = _title,
-            Description = _description
-        };
-        _itemsList.Add(itemToAdd);
+        _itemsListManager.AddNewItem(title, description);
+        
         UpdateListBox();
-    }
-
-    private void ImportItemsList()
-    {
-        string json;
-
-        if (!File.Exists("itemsList.json"))
-        {
-            return;
-        }
-
-        using (var r = new StreamReader("itemsList.json"))
-        {
-            json = r.ReadToEnd();
-        }
-
-        try
-        {
-            _itemsList = JsonSerializer.Deserialize<List<ToDoItem>>(json) ?? new List<ToDoItem>();
-        }
-        catch
-        {
-            throw new Exception("Invalid file format");
-        }
     }
 
     private void SaveItemsList(object sender, RoutedEventArgs e)
     {
-        string json = JsonSerializer.Serialize(_itemsList, new JsonSerializerOptions { WriteIndented = true });
-
-        using StreamWriter file = File.CreateText("itemsList.json");
-
-        file.Write(json);
+        _itemsListManager.SaveItemsList();
     }
+    
+    #endregion MethodsCalledFromWindow
+
+    #region MethodsCalledFromCode
+    
+    private string GetSelectedItemTitle()
+    {
+        if (listBox.SelectedIndex < 0)
+        {
+            return string.Empty;
+        }
+
+        var listBoxItem = listBox.SelectedItem.ToString();
+
+        return listBoxItem ?? string.Empty;
+    }
+    
+    private void ClearPreviewBox()
+    {
+        titleTextBox.Clear();
+        descriptionTextBox.Clear();
+        doneTextBox.Clear();
+    }
+    
+    private void UpdateListBox()
+    {
+        listBox.Items.Clear();
+        
+        if (_itemsListManager.ToDoItemsList.Count <= 0)
+        {
+            return;
+        }
+
+        foreach (ToDoItem item in _itemsListManager.ToDoItemsList)
+        {
+            listBox.Items.Add(item.Title);
+        }
+    }
+    
+    private void DrawItemDetails(ToDoItem toDoItem)
+    {
+        titleTextBox.Text = toDoItem.Title;
+        descriptionTextBox.Text = toDoItem.Description;
+        doneTextBox.Text = toDoItem.Done.ToString();
+
+        doneTextBox.Foreground = toDoItem.Done ? Brushes.Green : Brushes.Red;
+    }
+    
+    #endregion MethodsCalledFromCode
 }
